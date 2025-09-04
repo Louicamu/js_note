5ac32cd46fa7b67455b9f4de1e1ea908d67b8845
loui <1714425498@qq.com>
2025-09-04 22:59:28 +0800

    函数柯里化练习

---
+++ 发布-订阅模式练习.ts
@@ -0,0 +1,41 @@
+class EventBus1 {
+  private events: {
+    [key: string]: Array<{ fn: Function; isOnce: boolean }>;
+  };
+  constructor() {
+    this.events = {};
+  }
+  on(type: string, fn: Function, isOnce: boolean = false) {
+    const events = this.events;
+    if (events[type] == null) {
+      events[type] = [];
+    }
+    events[type].push({ fn, isOnce });
+  }
+  once(type: string, fn: Function) {
+    this.on(type, fn, true);
+  }
+  off(type: string, fn?: Function) {
+    if (!fn) {
+      this.events[type] = [];
+    } else {
+      const typeFnList = this.events[type];
+      if (typeFnList) {
+        this.events[type] = typeFnList.filter((typeFn) => typeFn.fn !== fn);
+      }
+    }
+  }
+  emit(type: string, ...args: any[]) {
+    const typeFnList = this.events[type];
+    if (typeFnList == null) return;
+    this.events[type] = typeFnList.filter((typeFn) => {
+      const { fn, isOnce } = typeFn;
+      fn(...args);
+      if (!isOnce) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
+}
